6d19415d3b403777c40d1af3bf7c97cf48723234
PHallin <petter-hallin99@live.se>
2023-11-28 11:35:50 +0100

    Applied Flyweight pattern

---
+++ Assets/Scripts/P1_Pooling/TreeData.cs
@@ -0,0 +1,17 @@
+﻿using P2_Flyweight;
+using UnityEngine;
+
+namespace P1_Pooling {
+    public class TreeData : MonoBehaviour {
+        public static TreeSeasonColors TreeSeasonColors { get; private set; }
+
+        private void Awake() {
+            LoadColourInfo();
+        }
+        
+        private static void LoadColourInfo() {
+            var fileContents = Resources.Load<TextAsset>("treeColors").text;
+            TreeSeasonColors = JsonUtility.FromJson<TreeSeasonColors>(fileContents);
+        }
+    }
+}
--- Assets/Scripts/P2_Flyweight/TreeSeasonColors.cs
+++ Assets/Scripts/P2_Flyweight/TreeSeasonColors.cs
@@ -1,32 +1,36 @@
 using System;
 using UnityEngine;
 
-[Serializable]
-public class TreeSeasonColors
-{
-    [SerializeField] private ColorInfo[] colors;
-    
-    /// <summary>
-    /// This returns the current color. The value changes every time
-    /// `MoveNext` is invoked.
-    /// </summary>
-    public Color CurrentColor
-    {
-        get
+namespace P2_Flyweight {
+    [Serializable]
+    public class TreeSeasonColors {
+        public ColorInfo[] colors;
+        private int _index;
+
+        /// <summary>
+        ///     OBSOLETE! This returns the current color. The value changes every time
+        ///     `MoveNext` is invoked.
+        /// </summary>
+        public Color CurrentColor
         {
-            var colorInfo = colors[_index];
+            get
+            {
+                var colorInfo = colors[_index];
+                return new Color(colorInfo.r, colorInfo.g, colorInfo.b, 1f);
+            }
+        }
+
+        /// <summary>
+        ///     OBSOLETE! This makes the Tree move on to the next color
+        /// </summary>
+        public void MoveNext() {
+            _index += 10;
+            _index %= colors.Length;
+        }
+
+        public Color GetColour(int index) {
+            var colorInfo = colors[index];
             return new Color(colorInfo.r, colorInfo.g, colorInfo.b, 1f);
         }
     }
-
-    /// <summary>
-    /// This makes the Tree move on to the next color
-    /// </summary>
-    public void MoveNext()
-    {
-        _index += 10;
-        _index %= colors.Length;
-    }
-
-    private int _index;
 }
