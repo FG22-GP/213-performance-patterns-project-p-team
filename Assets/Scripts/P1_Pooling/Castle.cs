e1e2bbab34e3df988f37c57021b8c981af1a2ccd
Gustav Carlberg <55480495+Litene@users.noreply.github.com>
2023-11-28 11:23:05 +0100

    Pooling added

--- Assets/Scripts/P1_Pooling/Castle.cs
+++ Assets/Scripts/P1_Pooling/Castle.cs
@@ -1,53 +1,53 @@
+using System;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
-public class Castle : MonoBehaviour
-{
-    public Projectile Projectile;
+[RequireComponent(typeof(ObjectPool))] public class Castle : MonoBehaviour {
 
-    private Transform _target;
-    private int _enemyLayerMask;
-    private float _currentCooldown;
-    
-    const float _maxCooldown = 0.8f;
+	public Projectile Projectile;
+	public ObjectPool PoolRef;
 
+	private Transform _target;
+	private int _enemyLayerMask;
+	private float _currentCooldown;
 
-    void Start()
-    {
-        this._enemyLayerMask = LayerMask.GetMask("Enemy");
-    }
+	const float _maxCooldown = 0.8f;
+	private void Awake() => PoolRef = GetComponent<ObjectPool>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        AcquireTargetIfNecessary();
-        TryAttack();
-    }
+	void Start() {
+		this._enemyLayerMask = LayerMask.GetMask("Enemy");
+	}
 
-    void AcquireTargetIfNecessary()
-    {
-        if (this._target == null)
-        {
-            this._target = Physics2D.OverlapCircle(this.transform.position, 5f, this._enemyLayerMask)?.transform;
-        }
-    }
+	// Update is called once per frame
+	void Update() {
+		AcquireTargetIfNecessary();
+		TryAttack();
+	}
 
-    void TryAttack()
-    {
-        _currentCooldown -= Time.deltaTime;
-        if (this._target == null || !(_currentCooldown <= 0f)) return;
-        this._currentCooldown = _maxCooldown;
-        Attack();
-    }
+	void AcquireTargetIfNecessary() {
+		if (this._target == null) {
+			this._target = Physics2D.OverlapCircle(this.transform.position, 5f, this._enemyLayerMask)?.transform;
+		}
+	}
 
-    void Attack()
-    {
-        Instantiate(this.Projectile, this.transform.position, GetTargetDirection());
-    }
+	void TryAttack() {
+		_currentCooldown -= Time.deltaTime;
+		if (this._target == null || !(_currentCooldown <= 0f)) return;
+		this._currentCooldown = _maxCooldown;
+		Attack();
+	}
 
-    Quaternion GetTargetDirection()
-    {
-        var dir = this._target.transform.position - this.transform.position;
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
-        return Quaternion.AngleAxis(angle, Vector3.forward);
-    }
+	void Attack() {
+		Projectile tempProjectile = PoolRef.Pool.Get();
+		Transform transformRef;
+		(transformRef = tempProjectile.transform).rotation = GetTargetDirection();
+		transformRef.position = this.transform.position;
+	}
+
+	Quaternion GetTargetDirection() {
+		var dir = this._target.transform.position - this.transform.position;
+		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+		return Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
 }
